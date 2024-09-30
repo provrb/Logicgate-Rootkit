07263f38556bf735284514e849497506ccfb0600
unknown <ethanjamesoliveira@gmail.com>
2024-09-30 04:21:02 +0100

    Test creating a server using ServerInterface. Works

--- Source/servermain.cpp
+++ Source/servermain.cpp
@@ -1,3 +1,38 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include "../Headers/server.h"
+
+// Link with ws2_32.lib
+#pragma comment(lib, "ws2_32.lib")
+#define ADDR "logicgate-test.ddns.net"
+
 int main() {
-	return 0;
+    SOCKET ClientSocket = INVALID_SOCKET;
+    struct sockaddr_in serverAddr;
+
+    ServerInterface server(SocketTypes::TCP, 5454);
+
+    // Bind the socket
+
+    // Listen on the socket for incoming connections
+
+    SOCKET ListenSocket = server.GetServerDetails().sfd;
+
+    // Accept a client socket
+    ClientSocket = accept(ListenSocket, nullptr, nullptr);
+    if ( ClientSocket == INVALID_SOCKET ) {
+        std::cerr << "Accept failed: " << WSAGetLastError() << std::endl;
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Client connected!" << std::endl;
+
+    // Cleanup
+    closesocket(ClientSocket);
+    closesocket(ListenSocket);
+    WSACleanup();
+    return 0;
 }
