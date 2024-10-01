1e818a9338d3642c56ae54075f6afbfc46a0b967
unknown <ethanjamesoliveira@gmail.com>
2024-10-01 22:16:14 +0100

    Add more function pointers related to sockets

--- Headers/net_common.h
+++ Headers/net_common.h
@@ -31,19 +31,23 @@ typedef int    ( WINAPI* _recv )        ( SOCKET s, char* buf, int len, int flag
 typedef int    ( WINAPI* _recvfrom )    ( SOCKET s, char* buf, int len, int flags, sockaddr* from, int* fromlen );
 typedef int    ( WINAPI* _connect )     ( SOCKET s, const sockaddr* addr, int namelen );
 typedef int    ( WINAPI* _listen )      ( SOCKET s, int backlog );
+typedef int    ( WINAPI* _shutdown )    ( SOCKET s, int how );
+typedef SOCKET ( WINAPI* _accept )      ( SOCKET s, sockaddr* addr, int* addrlen );
 
 // Dynamically loaded functions from the winsock library
-inline _socket       CreateSocket  = nullptr;
-inline _WSAStartup   StartWSA      = nullptr;
-inline _WSACleanup   CleanWSA      = nullptr;
-inline _closesocket  CloseSocket   = nullptr;
-inline _bind         BindSocket    = nullptr;
-inline _sendto       SendTo        = nullptr;
-inline _send         Send          = nullptr;
-inline _recv         Receive       = nullptr;
-inline _recvfrom     ReceiveFrom   = nullptr;
-inline _connect      ConnectSocket = nullptr;
-inline _listen       SocketListen  = nullptr;
+inline _socket       CreateSocket   = nullptr;
+inline _WSAStartup   StartWSA       = nullptr;
+inline _WSACleanup   CleanWSA       = nullptr;
+inline _closesocket  CloseSocket    = nullptr;
+inline _bind         BindSocket     = nullptr;
+inline _sendto       SendTo         = nullptr;
+inline _send         Send           = nullptr;
+inline _recv         Receive        = nullptr;
+inline _recvfrom     ReceiveFrom    = nullptr;
+inline _connect      ConnectSocket  = nullptr;
+inline _listen       SocketListen   = nullptr;
+inline _shutdown     ShutdownSocket = nullptr;
+inline _accept       AcceptOnSocket = nullptr;
 
 namespace NetCommon
 {
